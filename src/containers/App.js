282cb1ff3084f28f17afcec1c0240b942a47b247
Igor Perzic <iperzic@yahoo.com>
2018-03-28 23:30:03 +0200

    Update App container component
    Removed create-react-app's boilerplate code and files and replaced it
    with a proper container component connecting it to store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import * as msgActions from '../actions/messages';
 
-export default App;
+import Chat from '../components/Chat';
+
+const mapStateToProps = state => ({
+  messages: state.messages,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onChatLoad: () => dispatch(msgActions.getMessages()),
+  onNewMessage: message => dispatch(msgActions.sendMessage(message)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Chat);
