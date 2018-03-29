7198e223a9af56ae4855ae888e5667bc5cfd780e
Igor Perzic <iperzic@yahoo.com>
2018-03-29 15:30:03 +0200

    Add App container component test

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import configureStore from 'redux-mock-store';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import TestStore from '../support/store';
+
+describe('App', () => {
+  let store;
+  beforeEach(() => {
+    store = configureStore()(TestStore.createMessagesStore());
+  });
+
+  describe('when the there are no messages', () => {
+    beforeEach(() => {
+      store = configureStore()(TestStore.createMessagesStore());
+    });
+    it('should show empty input and empty message list', () => {
+      const appContainer = shallow(<App store={store} />).dive();
+
+      expect(appContainer.getElement()).toMatchSnapshot();
+    });
+  });
+
+  describe('when the there are messages', () => {
+    beforeEach(() => {
+      store = configureStore()(TestStore.createMessagesStore(undefined, undefined, [{
+        id: 'abc', author: 'Bugs Bunny', message: 'Whats up doc?', timestamp: 'Thu Mar 15 2018', isOwn: false,
+      }]));
+    });
+    it('should show empty input and message list', () => {
+      const appContainer = shallow(<App store={store} />).dive();
+
+      expect(appContainer.getElement()).toMatchSnapshot();
+    });
+  });
 });
