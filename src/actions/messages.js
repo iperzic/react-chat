7fb47a96ffa57b2612111b8eb38f92590f45bf17
Igor Perzic <iperzic@yahoo.com>
2018-03-29 23:21:18 +0200

    Add new actions to messages action creators

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -4,7 +4,26 @@ export const getMessages = () => ({
   type: msgTypes.GET_MESSAGES,
 });
 
+export const getMessagesSuccess = data => ({
+  type: msgTypes.GET_MESSAGES_SUCCESS,
+  payload: { data },
+});
+
+export const getMessagesFailure = err => ({
+  type: msgTypes.GET_MESSAGES_FAILURE,
+  message: err.message,
+});
+
 export const sendMessage = message => ({
   type: msgTypes.SEND_MESSAGE,
   payload: { message },
+});
+
+export const sendMessageSuccess = () => ({
+  type: msgTypes.SEND_MESSAGE_SUCCESS,
+});
+
+export const sendMessageFailure = err => ({
+  type: msgTypes.SEND_MESSAGE_FAILURE,
+  message: err.message,
 });
--- src/actions/messages.spec.js
+++ src/actions/messages.spec.js
@@ -3,12 +3,37 @@ import * as msgTypes from './messages.const';
 
 describe('messages actions', () => {
   describe('#getMessages()', () => {
-    it('should return messages data fetched', () => {
+    it('should fetch messages', () => {
       const expectedAction = {
         type: msgTypes.GET_MESSAGES,
       };
 
       expect(actions.getMessages()).toMatchObject(expectedAction);
+    });
+  });
+
+  describe('#getMessagesSuccess()', () => {
+    it('should return messages data fetched', () => {
+      const data = [];
+      const expectedAction = {
+        type: msgTypes.GET_MESSAGES_SUCCESS,
+        payload: { data },
+      };
+
+      expect(actions.getMessagesSuccess(data)).toMatchObject(expectedAction);
+    });
+  });
+
+  describe('#getMessagesFailure()', () => {
+    it('should handle thrown get messages error', () => {
+      const message = 'some error';
+      const error = { message };
+      const expectedAction = {
+        type: msgTypes.GET_MESSAGES_FAILURE,
+        message,
+      };
+
+      expect(actions.getMessagesFailure(error)).toMatchObject(expectedAction);
     });
   });
 
@@ -21,6 +46,29 @@ describe('messages actions', () => {
       };
 
       expect(actions.sendMessage(message)).toMatchObject(expectedAction);
+    });
+  });
+
+  describe('#sendMessageSuccess()', () => {
+    it('should return new message', () => {
+      const expectedAction = {
+        type: msgTypes.SEND_MESSAGE_SUCCESS,
+      };
+
+      expect(actions.sendMessageSuccess()).toMatchObject(expectedAction);
+    });
+  });
+
+  describe('#sendMessageFailure()', () => {
+    it('should handle thrown send message error', () => {
+      const message = 'some error';
+      const error = { message };
+      const expectedAction = {
+        type: msgTypes.SEND_MESSAGE_FAILURE,
+        message,
+      };
+
+      expect(actions.sendMessageFailure(error)).toMatchObject(expectedAction);
     });
   });
 });
