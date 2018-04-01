366057997cdb52115b50d69eddc100a888bc6445
Igor Perzic <iperzic@yahoo.com>
2018-04-01 23:19:12 +0200

    Add config file
    Config file is containing the default parameters for fetching messages

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const author = 'Igor Perzic';
+export const token = 'M3K4rhsBr6nm';
+export const limit = 10;
+export const endpoint = 'https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0';
+export const since = 1521096352339;
